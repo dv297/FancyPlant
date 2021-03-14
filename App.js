404c9568c7982f550baec7e29517ec3671d365b3
Daniel Vu <dvv297@gmail.com>
2021-03-13 21:13:52 -0600

    Add React Navigation

--- App.js
+++ App.js
@@ -1,36 +1,24 @@
-import { StatusBar } from "expo-status-bar";
+import "react-native-gesture-handler";
 import React from "react";
 import Amplify from "aws-amplify";
 import { withAuthenticator } from "aws-amplify-react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
 import awsconfig from "./src/aws-exports";
-import { StyleSheet, Text, View, Image, Dimensions } from "react-native";
-import Logo from "./assets/logo.png";
+import HomeScreen from "./src/views/Home";
 
 Amplify.configure(awsconfig);
 
-let dimensions = Dimensions.get("window");
+const Stack = createStackNavigator();
 
 const App = () => {
   return (
-    <View style={styles.container}>
-      <Image style={styles.tinyLogo} source={Logo} alt="FancyPlant logo" />
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={HomeScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 };
 
 export default withAuthenticator(App);
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-  tinyLogo: {
-    width: dimensions.width,
-    height: Math.round((dimensions.width * 9) / 16),
-  },
-});
