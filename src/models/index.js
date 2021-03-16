a2a73e11bbf650b896bf2eeb159bcaa210ff9e86
Daniel Vu <dvv297@gmail.com>
2021-03-15 20:45:32 -0500

    Add ESLint (#9)

--- src/models/index.js
+++ src/models/index.js
@@ -1,17 +1,12 @@
 // @ts-check
-import { initSchema } from '@aws-amplify/datastore';
-import { schema } from './schema';
+import { initSchema } from "@aws-amplify/datastore";
+import { schema } from "./schema";
 
 const DataPointType = {
-  "TEMPERATURE": "TEMPERATURE",
-  "HUMIDITY": "HUMIDITY"
+  TEMPERATURE: "TEMPERATURE",
+  HUMIDITY: "HUMIDITY",
 };
 
 const { DataPoint, Device, User } = initSchema(schema);
 
-export {
-  DataPoint,
-  Device,
-  User,
-  DataPointType
-};
+export { DataPoint, Device, User, DataPointType };
--- src/models/schema.js
+++ src/models/schema.js
@@ -1,189 +1,169 @@
 export const schema = {
-    "models": {
-        "DataPoint": {
-            "name": "DataPoint",
-            "fields": {
-                "id": {
-                    "name": "id",
-                    "isArray": false,
-                    "type": "ID",
-                    "isRequired": true,
-                    "attributes": []
-                },
-                "type": {
-                    "name": "type",
-                    "isArray": false,
-                    "type": {
-                        "enum": "DataPointType"
-                    },
-                    "isRequired": false,
-                    "attributes": []
-                },
-                "value": {
-                    "name": "value",
-                    "isArray": false,
-                    "type": "String",
-                    "isRequired": false,
-                    "attributes": []
-                },
-                "Device": {
-                    "name": "Device",
-                    "isArray": false,
-                    "type": {
-                        "model": "Device"
-                    },
-                    "isRequired": false,
-                    "attributes": [],
-                    "association": {
-                        "connectionType": "BELONGS_TO",
-                        "targetName": "dataPointDeviceId"
-                    }
-                }
-            },
-            "syncable": true,
-            "pluralName": "DataPoints",
-            "attributes": [
-                {
-                    "type": "model",
-                    "properties": {}
-                },
-                {
-                    "type": "auth",
-                    "properties": {
-                        "rules": [
-                            {
-                                "allow": "public",
-                                "operations": [
-                                    "create",
-                                    "update",
-                                    "delete",
-                                    "read"
-                                ]
-                            }
-                        ]
-                    }
-                }
-            ]
+  models: {
+    DataPoint: {
+      name: "DataPoint",
+      fields: {
+        id: {
+          name: "id",
+          isArray: false,
+          type: "ID",
+          isRequired: true,
+          attributes: [],
         },
-        "Device": {
-            "name": "Device",
-            "fields": {
-                "id": {
-                    "name": "id",
-                    "isArray": false,
-                    "type": "ID",
-                    "isRequired": true,
-                    "attributes": []
-                },
-                "name": {
-                    "name": "name",
-                    "isArray": false,
-                    "type": "String",
-                    "isRequired": true,
-                    "attributes": []
-                },
-                "Users": {
-                    "name": "Users",
-                    "isArray": true,
-                    "type": {
-                        "model": "User"
-                    },
-                    "isRequired": false,
-                    "attributes": [],
-                    "isArrayNullable": true,
-                    "association": {
-                        "connectionType": "HAS_MANY",
-                        "associatedWith": "deviceID"
-                    }
-                }
-            },
-            "syncable": true,
-            "pluralName": "Devices",
-            "attributes": [
-                {
-                    "type": "model",
-                    "properties": {}
-                },
-                {
-                    "type": "auth",
-                    "properties": {
-                        "rules": [
-                            {
-                                "allow": "public",
-                                "operations": [
-                                    "create",
-                                    "update",
-                                    "delete",
-                                    "read"
-                                ]
-                            }
-                        ]
-                    }
-                }
-            ]
+        type: {
+          name: "type",
+          isArray: false,
+          type: {
+            enum: "DataPointType",
+          },
+          isRequired: false,
+          attributes: [],
         },
-        "User": {
-            "name": "User",
-            "fields": {
-                "id": {
-                    "name": "id",
-                    "isArray": false,
-                    "type": "ID",
-                    "isRequired": true,
-                    "attributes": []
-                },
-                "deviceID": {
-                    "name": "deviceID",
-                    "isArray": false,
-                    "type": "ID",
-                    "isRequired": false,
-                    "attributes": []
-                }
-            },
-            "syncable": true,
-            "pluralName": "Users",
-            "attributes": [
-                {
-                    "type": "model",
-                    "properties": {}
-                },
-                {
-                    "type": "key",
-                    "properties": {
-                        "name": "byDevice",
-                        "fields": [
-                            "deviceID"
-                        ]
-                    }
-                },
-                {
-                    "type": "auth",
-                    "properties": {
-                        "rules": [
-                            {
-                                "allow": "public",
-                                "operations": [
-                                    "create",
-                                    "update",
-                                    "delete",
-                                    "read"
-                                ]
-                            }
-                        ]
-                    }
-                }
-            ]
-        }
+        value: {
+          name: "value",
+          isArray: false,
+          type: "String",
+          isRequired: false,
+          attributes: [],
+        },
+        Device: {
+          name: "Device",
+          isArray: false,
+          type: {
+            model: "Device",
+          },
+          isRequired: false,
+          attributes: [],
+          association: {
+            connectionType: "BELONGS_TO",
+            targetName: "dataPointDeviceId",
+          },
+        },
+      },
+      syncable: true,
+      pluralName: "DataPoints",
+      attributes: [
+        {
+          type: "model",
+          properties: {},
+        },
+        {
+          type: "auth",
+          properties: {
+            rules: [
+              {
+                allow: "public",
+                operations: ["create", "update", "delete", "read"],
+              },
+            ],
+          },
+        },
+      ],
     },
-    "enums": {
-        "DataPointType": {
-            "name": "DataPointType",
-            "values": [
-                "TEMPERATURE",
-                "HUMIDITY"
-            ]
-        }
+    Device: {
+      name: "Device",
+      fields: {
+        id: {
+          name: "id",
+          isArray: false,
+          type: "ID",
+          isRequired: true,
+          attributes: [],
+        },
+        name: {
+          name: "name",
+          isArray: false,
+          type: "String",
+          isRequired: true,
+          attributes: [],
+        },
+        Users: {
+          name: "Users",
+          isArray: true,
+          type: {
+            model: "User",
+          },
+          isRequired: false,
+          attributes: [],
+          isArrayNullable: true,
+          association: {
+            connectionType: "HAS_MANY",
+            associatedWith: "deviceID",
+          },
+        },
+      },
+      syncable: true,
+      pluralName: "Devices",
+      attributes: [
+        {
+          type: "model",
+          properties: {},
+        },
+        {
+          type: "auth",
+          properties: {
+            rules: [
+              {
+                allow: "public",
+                operations: ["create", "update", "delete", "read"],
+              },
+            ],
+          },
+        },
+      ],
     },
-    "nonModels": {},
-    "version": "43e3f48c4edc372cdd046fff164a943a"
+    User: {
+      name: "User",
+      fields: {
+        id: {
+          name: "id",
+          isArray: false,
+          type: "ID",
+          isRequired: true,
+          attributes: [],
+        },
+        deviceID: {
+          name: "deviceID",
+          isArray: false,
+          type: "ID",
+          isRequired: false,
+          attributes: [],
+        },
+      },
+      syncable: true,
+      pluralName: "Users",
+      attributes: [
+        {
+          type: "model",
+          properties: {},
+        },
+        {
+          type: "key",
+          properties: {
+            name: "byDevice",
+            fields: ["deviceID"],
+          },
+        },
+        {
+          type: "auth",
+          properties: {
+            rules: [
+              {
+                allow: "public",
+                operations: ["create", "update", "delete", "read"],
+              },
+            ],
+          },
+        },
+      ],
+    },
+  },
+  enums: {
+    DataPointType: {
+      name: "DataPointType",
+      values: ["TEMPERATURE", "HUMIDITY"],
+    },
+  },
+  nonModels: {},
+  version: "43e3f48c4edc372cdd046fff164a943a",
 };
