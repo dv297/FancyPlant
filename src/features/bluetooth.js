2e521af52aeb5ed4ff4698c8c654282ced968550
Daniel Vu <dvv297@gmail.com>
2021-03-27 11:19:32 -0500

    Use library for Bluetooth Classic

--- src/features/bluetooth.js
+++ src/features/bluetooth.js
@@ -1,13 +1,70 @@
-import { createSlice } from "@reduxjs/toolkit";
+/* eslint-disable no-param-reassign, no-console */
+
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+import RNBluetoothClassic from "react-native-bluetooth-classic";
+
+const scanForDevicesThunk = createAsyncThunk(
+  "bluetooth/scanForDevices",
+  async (_, thunkApi) => {
+    const { scanState, currentRequestId } = thunkApi.getState().bluetooth;
+
+    if (scanState !== "pending" || currentRequestId !== thunkApi.requestId) {
+      return;
+    }
+
+    const unpairedDevices = await RNBluetoothClassic.startDiscovery();
+    console.log(unpairedDevices);
+    return unpairedDevices.map(({ name, address }) => ({ name, address }));
+  }
+);
 
 const bluetoothSlice = createSlice({
-  name: "counter",
-  initialState: 0,
+  name: "bluetooth",
+  initialState: {
+    unpairedDevices: [],
+    scanState: "idle",
+    currentRequestId: null,
+    error: null,
+  },
   reducers: {
-    increment: (state, action) => state + action.payload,
-    decrement: (state, action) => state - action.payload,
+    stopScan: () => {
+      console.log("TODO: Stopping scan...");
+    },
+  },
+  extraReducers: {
+    [scanForDevicesThunk.pending]: (state, action) => {
+      if (state.scanState === "idle") {
+        state.scanState = "pending";
+        state.currentRequestId = action.meta.requestId;
+      }
+    },
+    [scanForDevicesThunk.fulfilled]: (state, action) => {
+      if (
+        state.scanState === "pending" &&
+        state.currentRequestId === action.meta.requestId
+      ) {
+        state.scanState = "idle";
+        state.unpairedDevices = action.payload;
+        state.currentRequestId = undefined;
+      }
+    },
+    [scanForDevicesThunk.rejected]: (state, action) => {
+      if (
+        state.scanState === "loading" &&
+        state.currentRequestId === action.meta.requestId
+      ) {
+        state.scanState = "idle";
+        state.error = action.error;
+        state.currentRequestId = undefined;
+      }
+    },
   },
 });
 
-export const { increment, decrement } = bluetoothSlice.actions;
+export const {
+  // scanForDevices,
+  pollForDevices,
+  stopScan,
+} = bluetoothSlice.actions;
+export const scanForDevices = scanForDevicesThunk;
 export default bluetoothSlice.reducer;
